ea876e54a5fcd71ac7743ea0529976effde2a8f8
pray-lee <praycis@gmail.com>
2019-01-23 15:22:29 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,18 @@
   </div>
 </template>
 <script>
-  import getQueryParams from './util/getQueryString'
-  import Listener from './util/listener'
-  export default {
-    name: 'App',
-    created(){
-      let type = getQueryParams('type')
-      Listener.token = getQueryParams('token')
-      if(type==1){
-        this.$router.push({path: '/'})
-      }else{
-        this.$router.push({path: '/record'})
-      }
+import getQueryParams from './util/getQueryString'
+import Listener from './util/listener'
+export default {
+  name: 'App',
+  created () {
+    let type = getQueryParams('type')
+    Listener.token = getQueryParams('token')
+    if (type == 1) {
+      this.$router.push({ path: '/' })
+    } else {
+      this.$router.push({ path: '/record' })
     }
   }
+}
 </script>
-
-
